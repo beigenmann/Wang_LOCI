762b9e0491b53231481caa6da1517c77a959d07a
Benno Eigenmann <beigenmann@gmail.com>
2023-05-15 05:58:59 +0200

    Add files via upload

--- LOCI.py
+++ LOCI.py
@@ -1,36 +1,32 @@
 from array import *
 import math
 
- #Wang LOCI - Rechnen mit Logarithmen
+# Wang LOCI - Rechnen mit Logarithmen
 
 # Der LOCI enthält also im Speicher die natürlichen Logarithmen dieser Werte
-T = [[0.1, -math.log(0.1)], 
-    [2,  -math.log(2)], 
-    [0.9,  -math.log(0.9)], 
-    [1.01,  -math.log(1.01)],
-    [0.999,  -math.log(0.999)],
-    [1.0001,  -math.log(1.0001)],
-    [0.9999999,  -math.log(0.9999999)],
-    [1.00000001,  -math.log(1.00000001)]]
-    
+T = [[0.1, -math.log(0.1)],
+     [2,  -math.log(2)],
+     [0.9,  -math.log(0.9)],
+     [1.01,  -math.log(1.01)],
+     [0.999,  -math.log(0.999)],
+     [1.0001,  -math.log(1.0001)],
+     [0.9999999,  -math.log(0.9999999)],
+     [1.00000001,  -math.log(1.00000001)]]
 
 
-imput_ = 1800
-calc = imput_
-sum = 0
+def lnfunc(imput_):
+    calc = imput_
+    sum = 0
+    for x in T:
+        if x[1] < 0:
+            while calc < 1:
+                calc = calc * x[0]
+                sum = sum + x[1]
+        else:
+            while calc > 1:
+                calc = calc * x[0]
+                sum = sum + x[1]
+    return sum
 
 
-for x in T:
-    if x[1] < 0 :
-        while calc < 1:
-            print( calc ," *" , x[0], " =",  calc * x[0], " ", x[1] )
-            calc = calc * x[0]
-            sum = sum + x[1]
-    else:
-        while calc > 1:
-            print( calc ," *" , x[0], " =",  calc * x[0], " ", x[1] )
-            calc = calc * x[0]
-            sum = sum + x[1]
-print(sum)
-
-
+print(lnfunc(18))
